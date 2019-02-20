ca89209c9b70e0deeaca7cca9789d743fb869d00
Terence1991 <cplterencehawes@gmail.com>
2019-02-20 17:21:24 +0000

    wired auth to component

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client', () => {
+      window.gapi.client.init({
+        clientId: '546110512453-8hg7ucf4350cd3g8dgtuhkhub7vpitiq.apps.googleusercontent.com',
+        scope: 'email'
+      })
+    })
+  }
+
   render() {
     return (
       <div>
